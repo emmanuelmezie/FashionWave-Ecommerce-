cb9b35f2a4c8f2027588f3c2e9ffd7c420daba89
Chime Emmanuel <irozuruchimezerem@gmail.com>
2018-05-09 17:30:51 +0100

    product brand error

--- action.php
+++ action.php
@@ -20,24 +20,6 @@ if(isset($_POST["category"])){
 		echo "</div>";
 	}
 }
-if(isset($_POST["brand"])){
-	$brand_query = "SELECT * FROM brands";
-	$run_query = mysqli_query($con,$brand_query);
-	echo "
-		<div class='nav nav-pills nav-stacked'>
-			<li class='active'><a href='#'><h4>Brands</h4></a></li>
-	";
-	if(mysqli_num_rows($run_query) > 0){
-		while($row = mysqli_fetch_array($run_query)){
-			$bid = $row["brand_id"];
-			$brand_name = $row["brand_title"];
-			echo "
-					<li><a href='#' class='selectBrand' bid='$bid'>$brand_name</a></li>
-			";
-		}
-		echo "</div>";
-	}
-}
 if(isset($_POST["page"])){
 	$sql = "SELECT * FROM products";
 	$run_query = mysqli_query($con,$sql);
@@ -63,7 +45,6 @@ if(isset($_POST["getProduct"])){
 		while($row = mysqli_fetch_array($run_query)){
 			$pro_id    = $row['product_id'];
 			$pro_cat   = $row['product_cat'];
-			$pro_brand = $row['product_brand'];
 			$pro_title = $row['product_title'];
 			$pro_price = $row['product_price'];
 			$pro_image = $row['product_image'];
@@ -97,7 +78,6 @@ if(isset($_POST["getFeaturedProduct"])){
 		while($row = mysqli_fetch_array($run_query)){
 			$pro_id    = $row['product_id'];
 			$pro_cat   = $row['product_cat'];
-			$pro_brand = $row['product_brand'];
 			$pro_title = $row['product_title'];
 			$pro_price = $row['product_price'];
 			$pro_image = $row['product_image'];
@@ -130,7 +110,6 @@ if(isset($_POST["get_seleted_Category"]) || isset($_POST["search"])){
 	while($row=mysqli_fetch_array($run_query)){
 			$pro_id    = $row['product_id'];
 			$pro_cat   = $row['product_cat'];
-			$pro_brand = $row['product_brand'];
 			$pro_title = $row['product_title'];
 			$pro_price = $row['product_price'];
 			$pro_image = $row['product_image'];
